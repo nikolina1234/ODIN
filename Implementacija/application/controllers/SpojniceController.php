3df243f403e89a2d853e7be83e8a97b5ec308616
Ognjen Petkovic <35452517+pognjen@users.noreply.github.com>
2019-05-30 12:48:44 +0200

    Add files via upload

--- Implementacija/application/controllers/SpojniceController.php
+++ Implementacija/application/controllers/SpojniceController.php
@@ -28,11 +28,19 @@ class SpojniceController extends CI_Controller
         $spojnica_opis = $this->SpojniceModel->getSpojnice();
 
         $spojnice = $this->SpojniceModel->getSpojniceS($spojnica_opis->id_spojnice);
+
+        $levo = array($spojnice[0],$spojnice[2],$spojnice[4],$spojnice[6],$spojnice[8]);
+
+        $desno = array($spojnice[1],$spojnice[3],$spojnice[5],$spojnice[7],$spojnice[9]);
+
+        shuffle($desno);
+
         session_start();
 
-        $_SESSION['opis'] = $spojnica_opis;
+        $_SESSION['opis'] = $spojnica_opis->opis_spojnice;
 
-        $_SESSION['spojnice'] = $spojnice;
+        $_SESSION['spojniceLevo'] = $levo;
+        $_SESSION['spojniceDesno'] = $desno;
 
         $this->view('SpojniceView');
     }
