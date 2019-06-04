3a78a8efcc6f63da59f0661f7190b899b2e85a17
Ognjen Petkovic <35452517+pognjen@users.noreply.github.com>
2019-06-04 23:35:59 +0200

    Ko Zna Zna Controller

--- application/controllers/KoZnaZnaController.php
+++ application/controllers/KoZnaZnaController.php
@@ -10,16 +10,43 @@
 
            session_start();
 
-           $_SESSION['koznazna'] = $niz[0];
+           $_SESSION['koznazna0'] = $niz[0];
+           $_SESSION['koznazna1'] = $niz[1];
+           $_SESSION['koznazna2'] = $niz[2];
+           $_SESSION['koznazna3'] = $niz[3];
+           $_SESSION['koznazna4'] = $niz[4];
 
-           
-            $odgovori = array($niz[0]->tacan,$niz[0]->netacan1,$niz[0]->netacan2,$niz[0]->netacan3);
+            $odgovori0 = array($niz[0]->tacan,$niz[0]->netacan1,$niz[0]->netacan2,$niz[0]->netacan3);
 
-            shuffle($odgovori);
+            shuffle($odgovori0);
 
-            $_SESSION['odgovori'] = $odgovori;
+            $_SESSION['odgovori0'] = $odgovori0;
 
-           $this->view('KoZnaZnaView');
+            $odgovori1 = array($niz[1]->tacan,$niz[1]->netacan1,$niz[1]->netacan2,$niz[1]->netacan3);
+
+            shuffle($odgovori1);
+
+            $_SESSION['odgovori1'] = $odgovori1;
+
+            $odgovori2 = array($niz[2]->tacan,$niz[2]->netacan1,$niz[2]->netacan2,$niz[2]->netacan3);
+
+            shuffle($odgovori2);
+
+            $_SESSION['odgovori2'] = $odgovori2;
+
+            $odgovori3 = array($niz[3]->tacan,$niz[3]->netacan1,$niz[3]->netacan2,$niz[3]->netacan3);
+
+            shuffle($odgovori3);
+
+            $_SESSION['odgovori3'] = $odgovori3;
+
+            $odgovori4 = array($niz[4]->tacan,$niz[4]->netacan1,$niz[4]->netacan2,$niz[4]->netacan3);
+
+            shuffle($odgovori4);
+
+            $_SESSION['odgovori4'] = $odgovori4;
+
+             $this->view('KoZnaZnaView');
         }
        
         public function view($page)
@@ -32,6 +59,19 @@
             $this->load->view('inc/header');
             $this->load->view($page,[]);
             $this->load->view('inc/footer');
+        }
+
+        public function check()
+        {
+            $odgovor = $_POST['odgovor'];
+
+            $pitanje = $_POST['pitanje'];
+
+            $this->load->model('KoZnaZnaModel');
+
+            echo $this->KoZnaZnaModel->proveri($odgovor,$pitanje);
+
+            
         }
 
     }
