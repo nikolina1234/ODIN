fc416baf6a1bf8bec97914cb034654db254bb64f
Ognjen Petkovic <35452517+pognjen@users.noreply.github.com>
2019-06-04 23:36:25 +0200

    Ko Zna Zna model

--- application/models/KoZnaZnaModel.php
+++ application/models/KoZnaZnaModel.php
@@ -10,37 +10,62 @@
 
        public function getKoZnaZna()
        {    
+
+            $alreadyPut = array();
+            $i = 0;
             $retVal = array();
 
-            for ($i=0;$i<1;$i++)
+          
+            for ($i=0;$i<5;$i++)
             {
 
             
                 $maxid = 0;
+                $minid = 0;
+
                 $query = $this->db->query("SELECT MAX(id_pitanje) as maxid FROM pitanje;");
 
                     foreach($query->result() as $row)
                     {
                         $maxid = $row->maxid;    
                     }
+
+                    $query = $this->db->query("SELECT MIN(id_pitanje) as minid FROM pitanje;");
+
+                    foreach($query->result() as $row)
+                    {
+                        $minid = $row->minid;    
+                    }
                 
-                $id = rand(1,$maxid);
+                
+                $id = rand($minid,$maxid);
                 
                 $query = $this->db->query("SELECT * FROM pitanje WHERE id_pitanje = $id");
                 
-                while ($query->result()==null)
+                while ($query->result()==null || in_array($id,$alreadyPut))
                 {
-                    $id = rand(1,$maxid);
+                    $id = rand($minid,$maxid);
                 
                     $query = $this->db->query("SELECT * FROM pitanje WHERE id_pitanje = $id");
-                }             
+                }          
+                
+                    $alreadyPut[$i] = $id;
                    foreach($query->result() as $row)
                     {
-                       $retVal[0] = $row;
+                       $retVal[$i] = $row;
                     }
          }
 
          return $retVal;
+       }
+
+       public function proveri($odgovor,$pitanje)
+       {
+            $query = $this->db->query("SELECT * FROM pitanje WHERE pitanje='$pitanje' and tacan = '$odgovor' ");
+
+            if ($query->result()==null) return false;
+            else   
+                return true;
        }
     }
 
