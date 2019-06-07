72de4ea95032929811df3598f8f0ed71137f3943
Igoresku <48559573+Igoresku@users.noreply.github.com>
2019-06-07 19:44:49 +0200

    Update AdminModel.php

--- application/models/AdminModel.php
+++ application/models/AdminModel.php
@@ -1,13 +1,23 @@
 <?php
-
+    /*
+		Author: Igor Duric 0646/2016
+	*/
     class AdminModel extends CI_Model
     {
+        /*
+            kreiram novu instancu i vezujem bazu
+        */
         public function __construct()
         {
             parent::__construct(); 
             $this->load->database();
         }
 
+        /*
+            Uzimam reci za slagalicu iz baze i vracam ih za prikaz
+            
+            @return niz reci
+        */
         public function getWords()
         {
             $query = $this->db->query("SELECT * FROM Slagalica");
@@ -20,6 +30,11 @@
             return $niz;
         }
 
+        /*
+            Uzimam sve moderatore i vracam ih za prikaz
+            
+            @return niz moderatora
+        */        
         public function getModerators()
         {
             $query = $this->db->query("SELECT * FROM Korisnik WHERE je_moderator = 1");
@@ -31,12 +46,22 @@
             }
              return $niz;
         }
-
+        
+        /*
+            Korisniku se ukidaju privilegije moderatora
+            
+            @args : modname - identifikacija moderatora 
+        */
         public function deleteModerator($modname)
         {
             $query = $this->db->query("UPDATE Korisnik SET je_moderator = 0 WHERE korisnicko_ime = '$modname'");
         }
 
+        /*
+            Uzimam sve registrovane korisnike iz baze i vracam ih za prikaz 
+            
+            @return niz korisnika
+        */
         public function getRegistrovani()
         {
             $query = $this->db->query("SELECT * FROM Korisnik WHERE je_admin = 0");
@@ -49,11 +74,21 @@
              return $niz;
         }
 
+        /*
+            Brisem registrovanog korisnika iz baze
+            
+            @args : korisnik - identifikacija korisnika
+        */
         public function deleteRegistrovani($korisnik)
         {
             $query = $this->db->query("DELETE FROM Korisnik WHERE korisnicko_ime = '$korisnik'");
         }
 
+        /*
+            Uzimam sve pojmove za spojnicu i vracam ih na prikaz 
+            
+            @return niz pojmova
+        */
         public function getSpojnice()
         {
             $query = $this->db->query("SELECT * FROM Spojnice");
@@ -77,6 +112,11 @@
             return $niz;
         } 
 
+        /*
+            Uzimam sva pitanja za ko zna zna i vracam ih na prikaz 
+            
+            @return niz pitanja
+        */
         public function getKoZnaZna()
         {
             $query = $this->db->query("SELECT * FROM Pitanje");
@@ -90,6 +130,11 @@
             return $pitanja;
         }
 
+        /*
+            Uzimam sve asocijacije i vracam ih na prikaz
+            
+            @return niz asocijacija
+        */
         public function getAsocijacije()
         {
             $query = $this->db->query("SELECT * FROM Asocijacija");
@@ -103,6 +148,11 @@
             return $asoc;
         }
 
+        /*
+            Uzimam sve zahteve korisnika za moderatora i vracam ih na prikaz 
+            
+            @return niz zahteva korisnika
+        */
         public function getZahtevi()
         {
             $query = $this->db->query("SELECT * FROM Korisnik WHERE zeli_mod = 1");
@@ -116,11 +166,21 @@
             return $korisnici;
         }
 
+        /*
+            Prihvata se zahtev korisnika da postane moderator
+            
+            @args : korisnik - identifikacija korisnika
+        */ 
         public function prihvatiMod($korisnik)
         {
             $query = $this->db->query("UPDATE Korisnik SET zeli_mod=0,je_moderator=1 WHERE korisnicko_ime = '$korisnik'");
         }
 
+        /*
+            Odbija se zahtev korisnika da postane moderator
+            
+            @args : korisnik - identifikacija korisnika
+        */
         public function odbijMod($korisnik)
         {
             $query = $this->db->query("UPDATE Korisnik SET zeli_mod=0 WHERE korisnicko_ime = '$korisnik'");
